70b94aedfdbfa60870fd458dd5338ef793f3f381
Francescolapiana <Lushika1219@gmail.com>
2022-10-06 18:05:24 +0200

    Finished

--- js/script.js
+++ js/script.js
@@ -1,22 +1,44 @@
-
+let sale
 let km = prompt('Quanti chilometri vuoi percorrere?');
 let age = prompt('Quanti anni hai?');
+
+if(isNaN(km) || isNaN(age)){
+    alert('Inserisci il valore in cifre')
+}
 
 let basePrice = km * 0.21;
 console.log(basePrice);
 
 if(age < 18){
-  let sale = 0.8;
+   sale = 0.8;
 }
 else if(age > 65){
-    let sale = 0.6;
+   sale = 0.6;
 }
 else{
-    let sale = 1;
+   sale = 1;
 }
 
 let discountentPrice = basePrice * sale;
+var twoDecimal = discountentPrice.toFixed(2);
+console.log(twoDecimal);
 
-console.log(discountentPrice);
+document.getElementById('kmtxt').innerHTML = km;
+document.getElementById('costtxt').innerHTML = twoDecimal;
+
+if(km < 26){
+    document.getElementById("hgt").style.backgroundColor = "seagreen";
+}
+else if(km > 65){
+    document.getElementById("hgt").style.backgroundColor = "brown";
+}
+else{
+    document.getElementById("hgt").style.backgroundColor = "darkcyan";
+}
+
+
+
+
+
 
 
